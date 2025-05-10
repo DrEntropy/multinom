abc74be84809b2960a2d23f1744602ff4bb806ad
DrEntropy <DrEntropy@users.noreply.github.com>
2025-05-10 13:29:58 -0700

    simplify the example

--- random_eff_multinom.R
+++ random_eff_multinom.R
@@ -8,7 +8,7 @@ set.seed(42)
 # Parameters
 num_companies <- 40
 num_categories <- 4 # Categories: 0 (pivot), 1, 2, 3
-num_prev_categories <- 4
+
 
 # 1. Generate company random effects (pivot category 0 logit is 0)
 mean_effects <- c(0, 0, 0) # categories 1,2,3; 0 is baseline
@@ -32,27 +32,18 @@ company_df <- data.frame(
 data_list <- lapply(1:num_companies, function(i) {
   n_obs <- sample(1:10, 1)
 
-  # Random prev_type for each observation
-  prev_type <- sample(0:(num_prev_categories - 1), n_obs, replace = TRUE)
 
-  # Fixed effects of prev_type (pivot=0)
-  prev_type_effects <- matrix(c(
-    0,    0,    0,    # prev_type = 0 (reference)
-    0.5, -0.5,  0.2,  # prev_type = 1
-    -0.3,  0.6, -0.4,  # prev_type = 2
-    0.2, -0.2,  0.5   # prev_type = 3
-  ), byrow = TRUE, nrow = 4)
-  prev_type_effects <- cbind(0, prev_type_effects)
+
 
   # Generate observations
   obs <- lapply(1:n_obs, function(j) {
-    logits <- company_effects[i, ] + prev_type_effects[prev_type[j] + 1, ]
+    logits <- company_effects[i, ]
     probs <- exp(logits) / sum(exp(logits))
     type <- sample(0:(num_categories - 1), size = 1, prob = probs)
 
     data.frame(
       company = company_df$company[i],
-      prev_type = paste0("type_", prev_type[j]),
+
       type = paste0("type_", type)
     )
   })
@@ -67,7 +58,7 @@ head(df)
 
 # 3. Fit categorical model using brms
 fit <- brm(
-  formula = type ~ prev_type + (1 | type | company),
+  formula = type ~  (1 | type | company),
   data = df,
   family = categorical(),
   chains = 2, cores = 2, iter = 2000
